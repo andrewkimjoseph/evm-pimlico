7499162f549bec542d62cb00273fce976f5da4c1
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-06-28 12:03:27 +0000

    Refactor: Update network configuration and dependencies

--- config.ts
+++ config.ts
@@ -10,40 +10,32 @@ if (!process.env.PRIVATE_KEY) {
   throw new Error("PRIVATE_KEY is not defined");
 }
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY as Address;
-
-export const PRIVATE_ACCOUNT = privateKeyToAccount(PRIVATE_KEY);
-
 if (!process.env.PIMLICO_API_KEY) {
   throw new Error("PIMLICO_API_KEY is not defined");
 }
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY as Address;
+
+const PRIVATE_ACCOUNT = privateKeyToAccount(PRIVATE_KEY);
+
 const PIMLICO_API_KEY = process.env.PIMLICO_API_KEY;
-
-if (!process.env.DRPC_API_KEY) {
-  throw new Error("DRPC_API_KEY is not defined");
-}
-
-const DRPC_API_KEY = process.env.DRPC_API_KEY;
-
-const DRPC_URL = `https://lb.drpc.org/ogrpc?network=celo-alfajores&dkey=${DRPC_API_KEY}`
 
 export const CREATE2_FACTORY_ADDRESS =
   "0x4e59b44847b379578588920cA78FbF26c0B4956C" as Address;
 
-export const selectedChain = celoAlfajores;
+const selectedChain = celoAlfajores;
 
-export const PIMLICO_URL = `https://api.pimlico.io/v2/${selectedChain.id}/rpc?apikey=${PIMLICO_API_KEY}`;
+const PIMLICO_URL = `https://api.pimlico.io/v2/${selectedChain.id}/rpc?apikey=${PIMLICO_API_KEY}`;
 
 export const PUBLIC_CLIENT = createPublicClient({
   chain: selectedChain,
-  transport: http(DRPC_URL),
+  transport: http(),
 });
 
 export const PRIVATE_CLIENT = createWalletClient({
   account: PRIVATE_ACCOUNT,
   chain: selectedChain,
-  transport: http(DRPC_URL),
+  transport: http(),
 });
 
 export const getSmartAccountAndClient = async () =>{
@@ -88,5 +80,3 @@ export const getSmartAccountAndClient = async () =>{
       smartAccountClient
     }
 }
-
-
